67fffe5a1963825ef8d7aa87a26abf4520ea2567
Lounis <lounis@akawam.com>
2023-10-31 17:27:16 +0100

    BrewMate installer

--- brewmate.sh
+++ brewmate.sh
@@ -19,10 +19,24 @@
 usage="Usage: brewmate.sh [-h|--help] [-i|--install] [-u|--uninstall]"
 # Get absolute directory of the current file.
 currentDir="$(realpath $(dirname "$0"))"
+# System homebrew etc directory.
+systemHomebrewEtcDir="/opt/homebrew/etc"
+# BrewMate homebrew etc directory.
+brewMateHomebrewEtcDir="${currentDir}/brew/opt/homebrew/etc"
+
+# Define colors for output.
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+# Define indent for output.
+INDENT="    "
+
 # Check if brew is installed.
 if ! command -v brew &> /dev/null
 then
-    echo "Brew is not installed."
+    echo "${RED}Brew is not installed.${NC}"
     echo "Install brew first, using this command:"
     echo "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
     exit
@@ -31,8 +45,8 @@ fi
 # Check if parameter is given.
 if [[ $# -eq 0 ]]
 then
-    echo "No parameter given."
-    echo "$usage"
+    echo "${RED}No parameter given.${NC}"
+    echo "${YELLOW}$usage${NC}"
     exit
 fi
 
@@ -54,8 +68,8 @@ do
             shift
         ;;
         *)
-            echo "Invalid parameter."
-            echo "$usage"
+            echo "${RED}Invalid parameter.${NC}"
+            echo "${YELLOW}$usage${NC}"
             exit
         ;;
     esac
@@ -64,16 +78,16 @@ done
 # Check if install or uninstall parameter is given, but not both.
 if [[ $install == true && $uninstall == true ]]
 then
-    echo "Invalid parameter, cannot install and uninstall at the same time."
-    echo "$usage"
+    echo "${RED}Invalid parameter, cannot install and uninstall at the same time.${NC}"
+    echo "${YELLOW}$usage${NC}"
     exit
 fi
 
 # Check if install or uninstall parameter is given.
 if [[ $install != true && $uninstall != true ]]
 then
-    echo "No parameter given."
-    echo "$usage"
+    echo "${RED}No parameter given.${NC}"
+    echo "${YELLOW}$usage${NC}"
     exit
 fi
 
@@ -83,62 +97,74 @@ fi
 if [[ $install == true ]]
 then
     # Check if brewmate is already installed. 
-    # By checking if /opt/homebrew/etc/brewmate.conf exists.
-    if [[ -f "/opt/homebrew/etc/brewmate.conf" ]]
+    # By checking if brewmate.conf exists in target directory.
+    if [[ -f "$systemHomebrewEtcDir/brewmate.conf" ]]
     then
-        echo "BrewMate is already installed."
+        echo "${GREEN}BrewMate is already installed.${NC}"
+        echo "${GREEN}Nothing to do.${NC}"
         exit
     fi
 
+    # Message.
+    echo "${GREEN}Installing BrewMate...${NC}"
+
     # Create symbolic link for each folders and files entries in ./opt/brew/homebrew/etc in /opt/homebrew/etc.
-    for entry in ${currentDir}/brew/opt/homebrew/etc/*; 
+    for entry in ${brewMateHomebrewEtcDir}/*; 
     do 
-        # Echo message for entry installation.
-        echo "Installing $entry"
-        # Check if entry is a directory.
-        if [[ -d /opt/homebrew/etc/$(basename $entry) ]]
+        # Backup : If entry already exists in /opt/homebrew/etc.
+        if [[ -e ${systemHomebrewEtcDir}/$(basename $entry) ]]
         then
-            echo "$entry is a directory, skipping."
-            continue
-        else
-            # Echo message for entry installation.
-            echo "Installing $entry"
+            # Echo message for entry backup.
+            echo "${INDENT}${YELLOW}$(basename $entry) exists in target${NC}"
+            echo "${INDENT}${GREEN}Backup $(basename $entry)${NC}"
+            # Create backup of entry.
+            mv ${systemHomebrewEtcDir}/$(basename $entry) ${systemHomebrewEtcDir}/$(basename $entry).bak
         fi
+        # Echo message for entry installation.
+        echo "${INDENT}Installing $(basename $entry) in $systemHomebrewEtcDir"
         # Create symbolic link.
-        # -s: Create a symbolic link.
-        # -f: If the target file already exists, then unlink it so that the link may occur.
-        # -n: If the target_file or target_dir is a symbolic link, do not follow it.  This is most useful with the -f option, to replace a symlink which may point to a directory.
-        ln -s $entry /opt/homebrew/etc;
+        ln -s $entry $systemHomebrewEtcDir;
     done
     # Show message.
-    echo "BrewMate installed."
+    echo "${GREEN}BrewMate installed.${NC}"
 
 # Uninstall brewmate
 elif [[ $uninstall == true ]]
 then
     # Check if brewmate is already uninstalled. 
     # By checking if /opt/homebrew/etc/brewmate.conf exists.
-    if [[ ! -f "/opt/homebrew/etc/brewmate.conf" ]]
+    if [[ ! -f "$systemHomebrewEtcDir/brewmate.conf" ]]
     then
-        echo "BrewMate is not installed."
-        echo "Nothing to do."
+        echo "${YELLOW}BrewMate is not installed.${NC}"
+        echo "${YELLOW}Nothing to do.${NC}"
         exit
     fi
 
+    # Message.
+    echo "${GREEN}Uninstalling BrewMate...${NC}"
+
     # Remove symbolic link for each folders and files entries in ./opt/brew/homebrew/etc in /opt/homebrew/etc.
-    for entry in ${currentDir}/brew/opt/homebrew/etc/*;  
+    for entry in ${brewMateHomebrewEtcDir}/*;  
     do 
         # Check if entry is a symbolic link.
-        if [[ -L /opt/homebrew/etc/$(basename $entry) ]]
+        if [[ -L ${systemHomebrewEtcDir}/$(basename $entry) ]]
         then
             # Echo message for entry uninstallation.
-            echo "Uninstalling $entry"
+            echo "${INDENT}${YELLOW}Uninstalling $(basename $entry)"
             # Remove symbolic link.
-            rm /opt/homebrew/etc/$(basename $entry); 
+            rm ${systemHomebrewEtcDir}/$(basename $entry); 
+            # Restore : If backup of entry exists in /opt/homebrew/etc.
+            if [[ -e ${systemHomebrewEtcDir}/$(basename $entry).bak ]]
+            then
+                # Echo message for entry restore.
+                echo "${INDENT}${GREEN}Restoring backup of $(basename $entry)${NC}"
+                # Restore entry.
+                mv ${systemHomebrewEtcDir}/$(basename $entry).bak ${systemHomebrewEtcDir}/$(basename $entry)
+            fi
         else
-            echo "$entry is not a symbolic link, skipping."
+            echo "${INDENT}$(basename $entry) is not a symbolic link, skipping."
         fi
     done
     # Show message.
-    echo "BrewMate uninstalled."
+    echo "${GREEN}BrewMate uninstalled.${NC}"
 fi
