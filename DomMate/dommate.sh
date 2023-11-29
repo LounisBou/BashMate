d2f7dd91d89bf13e7f278d8b191b42c9ab7227e2
Lounis <lounis@akawam.com>
2023-11-29 11:33:35 +0100

    DomMate WIP

--- DomMate/dommate.sh
+++ DomMate/dommate.sh
@@ -93,7 +93,12 @@ elif [[ "$OS_TYPE" == "linux" ]]; then
         #   (https://unofficial-builds.nodejs.org/download/release/v16.15.0/ e.g. 
         #   Version 16.15.0 should work.
         # NOTE 2: For Ubuntu see tip below
-        sudo curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+        sudo apt-get update
+        sudo apt-get install -y ca-certificates curl gnupg
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+        sudo apt-get update
         sudo apt-get install -y nodejs git make g++ gcc
         # Verify that the correct nodejs and npm (automatically installed with nodejs)
         # version has been installed
