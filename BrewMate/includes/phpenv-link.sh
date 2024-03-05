d9056b51a902ba394a317a48738b3ca3ff53c063
Lounis <lounis@akawam.com>
2024-03-05 13:11:24 +0100

    Improve phpenv and phpbrew linker script

--- BrewMate/includes/phpenv-link.sh
+++ BrewMate/includes/phpenv-link.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 # Path: BrewMate/includes/phpenv-link.sh
 #
 # This script links the php installed by Brew to ~/.phpenv/versions
@@ -9,6 +9,14 @@
 # Inspired by yuki777/links-phps.bash
 # See https://gist.github.com/yuki777/6244823b8aa8cf4457e97e6407ada5ad
 #
+
+# Define colors
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+# Light blue
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
 
 # Exit with error if any command fails
 set -euo pipefail
@@ -32,34 +40,72 @@ fi
 
 # Function to create symlink
 create_symlink() {
+    # Arguments
     local target=$1
     local link_name=$2
     # Check target exists
     if [ ! -e "$link_name" ]; then
+        # Message
+        echo "Symlink not found: $link_name"
         # Create symlink
+        echo "${YELLOW}Creating symlink: $link_name"
         ln -s "$target" "$link_name" || {
-            echo "Failed to create symlink: $link_name from $target"
+            echo "${RED}    Failed to create symlink: $link_name from $target${NC}"
+            echo "${YELLOW}    $link_name is not a symlink, please remove it manually and re-run this script${NC}"
             return 1
         }
-        echo "Created symlink: $link_name"
+        echo ${GREEN}Created symlink: $link_name${NC}
     else
-        echo "Symlink already exists: $link_name"
+        # Remove symlink
+        echo "${YELLOW}Removing symlink: $link_name"
+        if [ -L "$link_name" ]; then
+            rm "$link_name" || {
+                echo "${RED}    Failed to remove symlink: $link_name${NC}"
+                return 1
+            }
+            echo "${GREEN}  Removed symlink: $link_name${NC}"
+        fi
+        # Create symlink
+        echo "${YELLOW}Creating symlink: $link_name${NC}"
+        ln -s "$target" "$link_name" || {
+            echo "${RED}    Failed to create symlink: $link_name from $target${NC}"
+            return 1
+        }
+        echo "${GREEN}  Created symlink: $link_name${NC}"
     fi
+    # Return success
+    return 0
 }
 
 # Create the versions directory if it doesn't exist
 echo "# Link the php installed by Brew to $PHPENV_DIR"
 for php_dir in "$CELLAR_DIR"/php*/[0-9]*.*; do
+    # Message
+    echo "${YELLOW}Checking: $php_dir${NC}"
     # Check php dir exists
     if [ ! -d "$php_dir" ]; then
         continue
     fi
+    echo "${GREEN}   Found: $php_dir${NC}"
     # Check php bin exists
     phpPath="$php_dir/bin/php"
     if [ ! -f "$phpPath" ]; then
         continue
     fi
+    echo "${GREEN}   Found: $phpPath${NC}"
+    # Get PHP version
+    echo "${YELLOW}Check version with command : $phpPath -r 'echo PHP_MAJOR_VERSION.\".\".PHP_MINOR_VERSION.\".\".PHP_RELEASE_VERSION;'${NC}"
+    version=$("$phpPath" -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION.".".PHP_RELEASE_VERSION;' 2>&1) || true
+    # Remove warning message, keep only last line
+    version=$(echo "$version" | tail -n 1)
+    echo "${GREEN}  Version : $version${NC}"
+    # If version is empty, skip
+    if [ -z "$version" ]; then
+        echo "${RED}    Failed to get version from $phpPath${NC}"
+        continue
+    fi
     # Create symlink
-    version=$("$phpPath" -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION.".".PHP_RELEASE_VERSION;')
     create_symlink "$php_dir" "$PHPENV_DIR/$version" || true
+    # Separator
+    echo "----------------------------------------"
 done
