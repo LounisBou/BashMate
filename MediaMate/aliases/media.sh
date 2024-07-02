7c935a9661c95945ea72ca62bb1c58e51fbf9cfa
LounisBou <lounis.bou@gmail.com>
2024-07-02 17:11:22 +0200

    MediaMate optimization

---
+++ MediaMate/aliases/media.sh
@@ -0,0 +1,20 @@
+# Aliases for media management
+# Created by LounisBou
+
+# GLOBAL VARIABLES
+
+# COMMANDS
+
+# ! Search media on disk
+
+# Search for a folder on disks media folders (Allow partial name search)
+function find-media(){
+    # Execute MediaMate/FindMedia/find-media.sh
+    source ~/BashMate/MediaMate/FindMedia/find-media.sh
+}
+
+# Create a cache file with all medias folders
+function index-medias(){
+    # Execute MediaMate/FindMedia/index-medias.sh
+    source ~/BashMate/MediaMate/FindMedia/index-medias.sh
+}
