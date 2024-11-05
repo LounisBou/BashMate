660cbd3a91da6ffc73afdc4506d74cc9403839ac
LounisBou <lounis.bou@gmail.com>
2024-11-05 17:21:25 +0100

    Change __main__.py to use path in params

--- FileMate/filemate/__main__.py
+++ FileMate/filemate/__main__.py
@@ -10,6 +10,8 @@ def main():
     """
     Entry point for the FileMate application.
     """
+    
+    # Create the parser
     parser = argparse.ArgumentParser(description='FileMate - A file management tool.')
     parser.add_argument('directory', type=Path, help='Path to the directory to process')
     parser.add_argument('--clean', action='store_true', required=False, help='Clean file names')
@@ -17,13 +19,14 @@ def main():
     parser.add_argument('--delete-empty', action='store_true', required=False, help='Delete empty subdirectories')
     parser.add_argument('--delete-duplicates', action='store_true', required=False, help='Delete duplicate files')
     parser.add_argument('--delete-small', action='store_true', required=False, help='Delete small files')
+    
+    # Parse the arguments
     args = parser.parse_args()
-
+    
+    # Directory path
     directory_path = args.directory
-    
-    
     # Path 
-    path = Path("/Volumes/IznoServer SSD/torrents")
+    path = Path(directory_path)
     # Directory
     directory = Directory(path)
     # Print the directory
