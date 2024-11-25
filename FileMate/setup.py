28d118e82f9a18fc51d6dbab2b1f8841f7102687
LounisBou <lounis.bou@gmail.com>
2024-11-25 14:21:36 +0100

    Update filemate setup.py

--- FileMate/setup.py
+++ FileMate/setup.py
@@ -7,17 +7,14 @@ setup(
     name='FileMate',                   
     version='0.0.1',                   
     author='LounisBou',                
-    author_email='lounis.bou@example.com',  
+    author_email='lounis.bou@gmail.com',  
     description='A file management tool for sorting and cleaning files.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/lounisbou/FileMate',  
     license='MIT',                      
     packages=find_packages(),
-    install_requires=[   
-        # 'numpy>=1.18.0',
-        # 'requests>=2.23.0',
-    ],
+    install_requires=[],
     entry_points={
         'console_scripts': [
             'filemate=filemate.__main__:main',  # Command-line entry point
